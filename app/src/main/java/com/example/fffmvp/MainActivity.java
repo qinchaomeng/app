8c58e9c8eb10559ee3b79b31c34a7b74f37f880b
秦超蒙 <1980057074@qq.com>
2019-04-11 19:32:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fffmvp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fffmvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
